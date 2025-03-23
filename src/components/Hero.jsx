cb4e8b5bbcf2a044d40796f6a168be07990494a3
sheikhmuhammadzain <zaingee320@gmail.com>
2025-03-24 01:16:55 +0500

    added winterdp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/dp.jpeg";
+import profilePic from "../assets/winterdp.jpg";
 import { motion } from 'framer-motion';
 
 const container = (delay)=>({
