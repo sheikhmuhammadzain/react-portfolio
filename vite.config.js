c6428530d72bd85c6403616fea310337876ac601
sheikhmuhammadzain <zaingee320@gmail.com>
2025-10-05 13:46:33 +0500

    applied images optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,63 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      logStats: true,
+      ansiColors: true,
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      includePublic: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                cleanupIds: {
+                  minify: false,
+                  remove: false,
+                },
+                convertPathData: false,
+              },
+            },
+          },
+          'sortAttrs',
+          {
+            name: 'addAttributesToSVGElement',
+            params: {
+              attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+            },
+          },
+        ],
+      },
+      png: {
+        quality: 85,
+        compressionLevel: 9,
+      },
+      jpeg: {
+        quality: 85,
+        progressive: true,
+      },
+      jpg: {
+        quality: 85,
+        progressive: true,
+      },
+      webp: {
+        quality: 85,
+        lossless: false,
+      },
+      avif: {
+        quality: 80,
+        lossless: false,
+      },
+      cache: true,
+      cacheLocation: 'node_modules/.cache/vite-image-optimizer',
+    }),
+  ],
 })
