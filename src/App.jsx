00858844d274ec1cad8365c6aad87e56c552d037
sheikhmuhammadzain <zaingee320@gmail.com>
2026-01-31 13:36:48 +0500

    remove cursor

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import SmoothCursor from "./components/SmoothCursor";
 import PerformanceMonitor from "./components/PerformanceMonitor";
 import Chatbot from "./components/Chatbot";
 import Blogs from "./pages/Blogs";
@@ -56,7 +55,7 @@ const App = () => {
           <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
         </div>
 
-        <SmoothCursor />
+
         <Chatbot />
         
         <div className="container mx-auto px-4 sm:px-8 max-w-full">
