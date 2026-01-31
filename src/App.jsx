dea7223804f130ff11e299b633867d96d0fa3491
sheikhmuhammadzain <zaingee320@gmail.com>
2026-01-31 14:36:39 +0500

    added fix

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const App = () => {
       {/* Keywords that rank in 2025 */}
       {/* Open Graph */}
       {/* Twitter Card */}
-      <div className="text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 min-h-screen overflow-hidden">
+      <div className="text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 min-h-screen overflow-x-hidden">
         <div className="fixed top-0 -z-10 h-full w-full">
           <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
         </div>
