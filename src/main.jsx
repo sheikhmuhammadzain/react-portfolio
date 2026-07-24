a253299eb4fa30e94a1de435fc96c2fe6b8abd2d
sheikhmuhammadzain <zaingee320@gmail.com>
2026-07-24 17:04:44 +0500

    asdfasdfasdf

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import SmoothScrollProvider from './components/SmoothScrollProvider.jsx'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <SmoothScrollProvider>
-        <App />
-      </SmoothScrollProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <SmoothScrollProvider>
+          <App />
+        </SmoothScrollProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
